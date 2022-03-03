72c61e23bbd4fd96a9044a5be0646651661956d8
Daniel Beattie <daniel.beattie@boomin.com>
2022-03-03 21:25:21 +0000

    add husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next';
-import MyToggle from '../components/Switch';
 
 const Home: NextPage = () => {
   return (
     <>
       <h1 className="text-5xl">Hello Dan</h1>
-      <MyToggle />
     </>
   );
 };
