a8f27cc28257043e0d377b50149a91ec7092dcac
Daniel Beattie <daniel.beattie@boomin.com>
2022-03-03 21:01:15 +0000

    linting working

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Switch } from "@headlessui/react";
+import React, { useState } from 'react';
+import { Switch } from '@headlessui/react';
 
 export default function MyToggle() {
   const [switchValue, setSwitchValue] = useState(false);
@@ -14,13 +14,13 @@ export default function MyToggle() {
             checked={switchValue}
             onChange={setSwitchValue}
             className={`${
-              switchValue ? "bg-indigo-600" : "bg-gray-200"
+              switchValue ? 'bg-indigo-600' : 'bg-gray-200'
             } relative inline-flex flex-shrink-0 h-6 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer w-11 focus:outline-none focus:shadow-outline`}
           >
             {({ checked }) => (
               <span
                 className={`${
-                  checked ? "translate-x-5" : "translate-x-0"
+                  checked ? 'translate-x-5' : 'translate-x-0'
                 } inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full`}
               />
             )}
